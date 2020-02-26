d9fe5eba4e4f4bd49ce5c8cb720b872a41218c81
alee0510 <ali.muksin0510@gmail.com>
2020-02-26 12:03:03 +0700

    bug fixed

---
+++ controllers/account.js
@@ -0,0 +1,83 @@
+// import module
+const bycript = require('bcryptjs')
+const jwt = require('../helpers/jwt')
+
+// setup connection
+const database = require('../database')
+const connection = require('../helpers/databaseQuery')(database)
+
+// export controllers
+module.exports = {
+    // get account info by user id => NEED login authentication
+    getAccountById : async (req, res) => {
+        await connection.databaseQueryWithErrorHandle(res, async () => {
+            const query = 'SELECT * FROM users WHERE id = ?'
+            const result = await connection.databaseQuery(query, [req.user.id])
+
+            // send feedback to client-side
+            res.status(200).send(result[0])
+        })
+    },
+    // edit account : change username or passowrd => NEED login authentication
+    changePassword : async (req, res) => {
+        const id = req.user.id
+        const { confirmPassword, newPassword } = req.body
+        await connection.databaseQueryWithErrorHandle(res, async () => {
+            // do query to get user data
+            const getUser = 'SELECT * FROM users WHERE id = ?'
+            const user = await connection.databaseQuery(getUser, id)
+
+            // do authentication => password confirmation
+            const valid = await bycript.compare(confirmPassword, user[0].password)
+            if (!valid) throw ({code : 400, msg : 'invalid password.'})
+            
+            // crypt new password
+            const salt = await bycript.genSalt(10)
+            const newCryptPassword = await bycript.hash(newPassword, salt)
+
+            // update password in database
+            const update = 'UPDATE users SET password = ? WHERE id = ?'
+            await connection.databaseQuery(update, [newCryptPassword, id])
+
+            // send feedback to client-side
+            res.status(200).send('password has been updated.')
+        })
+    },
+    changeUsername : async (req, res) => {
+        const { username } = req.body // new username
+        await connection.databaseQueryWithErrorHandle(res, async () => {
+            // check if new username is avaiable
+            const check = 'SELECT * FROM users WHERE username = ?'
+            const result = await connection.databaseQuery(check, [username])
+            if (result.length !== 0) throw ({code : 400, msg : 'username is already exist.'})
+            
+            // update username in database
+            const update = 'UPDATE users SET ? WHERE id = ?'
+            await connection.databaseQuery(update, [username, req.user.id])
+            
+            // send feedback to client-side
+            res.status(200).send('username has been updated.')
+        })
+    },
+    // delete account, NEED login authorization and password authentication
+    delete : async (req, res) => {
+        const id = req.user.id
+        await connection.databaseQueryWithErrorHandle(res, async () => {
+            // do query to get user data
+            const getUser = 'SELECT * FROM users WHERE id = ?'
+            const user = await connection.databaseQuery(getUser, id)
+    
+            // do authentication => password confirmation
+            const valid = await bycript.compare(confirmPassword, user[0].password)
+            if (!valid) throw ({code : 400, msg : 'invalid password.'})
+
+            // do query
+            const query = 'DELETE FROM users WHERE id = ?'
+            await connection.databaseQuery(query, id)
+
+            // send feedback to client-side
+            res.status(200).send('your account has been deleted.')
+        })
+    },
+
+}
--- controllers/users.js
+++ controllers/users.js
@@ -13,11 +13,7 @@ module.exports = {
     register : async (req, res) => {
         const { username, email, password } = req.body
         await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do input validation
-            const { error } = validation.validateRegister(req.body)
-            if (error) throw ({code : 400, msg : error.details[0].message})
-
-            // check username and email avaiability, username or email can't duplicate
+            // check if username and email is avaiable, username or email can't be duplicate
             const check = 'SELECT * FROM users WHERE username = ? OR email = ?'
             const result = await connection.databaseQuery(check, [username, email])
             if (result.length !== 0) throw ({code : 400, msg : 'username or email is already exist.'})
@@ -39,16 +35,12 @@ module.exports = {
     // login
     login : async (req, res) => {
         await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do input validation
-            const { error } = validation.validateLogin(req.body)
-            if (error) throw ({code : 400, msg : error.details[0].message})
-
             // do query
             const query = 'SELECT * FROM users WHERE username = ?'
             const result = await connection.databaseQuery(query, req.body.username)
             if (result.length === 0) throw ({code : 404, msg : 'user doesn\'t found.'})
 
-            // do authorization or password validation
+            // do password authentication
             const valid = await bycript.compare(req.body.password, result[0].password)
             if (!valid) throw({code : 400, msg : 'invalid password.'})
 
@@ -60,71 +52,17 @@ module.exports = {
             res.header('Auth-Token', token).send('Welcome.')
         })
     },
-    // change password
-    changePassword : async (req, res) => {
-        const id = parseInt(req.params.id)
-        const { confirmPassword, newPassword } = req.body
-        await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do query to get user data
-            const getUser = 'SELECT * FROM users WHERE id = ?'
-            const user = await connection.databaseQuery(getUser, id)
-
-            // do authorization, password confirmation
-            const valid = await bycript.compare(confirmPassword, user[0].password)
-            if (!valid) throw ({code : 400, msg : 'invalid password.'})
-            
-            // do input validation
-            const { error } = validation.validatePassword({newPassword})
-            if (error) throw ({code : 400, msg : error.details[0].message})
-            
-            // crypt new password
-            const salt = await bycript.genSalt(10)
-            const newCryptPassword = await bycript.hash(newPassword, salt)
-
-            // update password in database
-            const update = 'UPDATE users SET password = ? WHERE id = ?'
-            await connection.databaseQuery(update, [newCryptPassword, id])
-
-            // send feedback to client-side
-            res.status(200).send('password has been updated.')
-        })
-    },
-    // edit username
-    editUsername : async (req, res) => {
-        const id = parseInt(req.params.id)
-        const { username } = req.body // new username
-        await connection.databaseQueryWithErrorHandle(res, async () => {
-            // check new username avaiability
-            const check = 'SELECT * FROM users WHERE username = ?'
-            const result = await connection.databaseQuery(check, [username])
-            if (result.length !== 0) throw ({code : 400, msg : 'username is already exist.'})
-            
-            // do input validation
-            const { error } = validation.validateUsername({username})
-            if (error) throw ({code : 400, msg : error.details[0].message})
-            
-            // update username
-            const update = 'UPDATE users SET ? WHERE id = ?'
-            await connection.databaseQuery(update, [req.body, id])
-            
-            // send feedback to client-side
-            res.status(200).send('username has been updated.')
-        })
-    },
-    // delete account
-    delete : async (req, res) => {
-        const id = parseInt(req.params.id)
-        await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do query
-            const query = 'DELETE FROM users WHERE id = ?'
-            await connection.databaseQuery(query, id)
-
-            // send feedback to client-side
-            res.status(200).send('your account has been deleted.')
-        })
-    },
     // keep login
-    
+    stayLogin : async (req, res) => {
+        await connection.databaseQueryWithErrorHandle(res, async () => {
+            // do query to always provide data to user
+            const query = 'SELECT * FROM users WHERE id = ?'
+            const result = await connection.databaseQuery(query, req.user.id)
+
+            // send feedback to client-side
+            res.status(200).send(result[0])
+        })
+    }
 }
 
-// NOTE : validation input need to improved
+// NOTE : validation input has been done in client-side before sending a request
