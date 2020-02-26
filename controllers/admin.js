ceaec05ccbf5396d678b3414cff5acc03016a8ea
alee0510 <ali.muksin0510@gmail.com>
2020-02-26 14:57:24 +0700

    bugs fixed

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,100 +1,40 @@
-// import module
-// const bycript = require('bcryptjs')
-// const validation = require('../helpers/validation')
-// const jwt = require('../helpers/jwt')
-
 // setup connection
 const database = require('../database')
 const connection = require('../helpers/databaseQuery')(database)
 
 // export controllers
 module.exports = {
-    // get all admins data
-    getAdmins : async (req, res) => {
+    // superadmin features : access all users data and activity plus change user role
+    getUserDataIncludeAdmin : async (req, res) => {
         await connection.databaseQueryWithErrorHandle(res, async () => {
             // do authorization
-            if(req.user.role !== 'superadmin') throw ({code : 401, msg : 'access denied.'})
-            
-            // do query
-            const query = 'SELECT * FROM users WHERE role = ?'
-            const result = await connection.databaseQuery(query, 'admins')
+            if (req.user.role !== 1) throw ({code : 401, msg : 'access denied.'})
 
-            // send result to client-side
-            res.status(200).send(result)
-        })
-    },
-    // get all users data
-    getUsers : async (req, res) => {
-        await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do authorization
-            const role = ['admin', 'superadmin']
-            if (!role.includes(req.user.role)) throw ({code : 401, msg : 'access denied.'})
-
-            // do query
-            const query = 'SELECT * FROM users WHERE role = ?'
-            const result = await connection.databaseQuery(query, 'user')
-
-            // send result to client-side
-            res.status(200).send(result)
-        })
-    },
-    // get roles
-    getRoles : async (req, res) => {
-        await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do authorization
-            if(req.user.role !== 'superadmin') throw ({code : 401, msg : 'access denied.'})
-
-            // do query
-            const query = 'SELECT * FROM roles'
+            const query = `SELECT us.id, us.username, us.email, pf.name, 
+                            pf.birthdate, pf.phone, pf.address, us.role 
+                            FROM users us
+                            JOIN profiles pf ON us.id = pf.id
+                            WHERE us.role != 1`
             const result = await connection.databaseQuery(query)
 
             // send feedback to client-side
             res.status(200).send(result)
         })
     },
-    // superadmin : edit role
-    editRole : async (req, res) => {
-        const id = parseInt(req.params.id)
+    getUserDataOnly : async (req, res) => {
         await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do authorization
-            if (req.user.role !== 'superadmin') throw ({code : 401, msg : 'action restricted.'})
-
-            // do edit query
-            const edit = 'UPDATE users SET ? WHERE id = ?'
-            await connection.databaseQuery(edit, [req.body, id])
+            const query = `SELECT us.id, us.username, us.email, pf.name, 
+                            pf.birthdate, pf.phone, pf.address, us.role 
+                            FROM users us
+                            JOIN profiles pf ON us.id = pf.id
+                            WHERE us.role != 1 AND us.role != 2`
+            const result = await connection.databaseQuery(query)
 
             // send feedback to client-side
-            res.status(200).send('role has been changed.')
+            res.status(200).send(result)
         })
-    },
-    // superadmin : delete admin (single or multiple)
-    singleDelete : async (req, res) => {
-        const id = parseInt(req.params.id)
-        await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do authorization
-            if (req.user.role !== 'superadmin') throw ({code : 401, msg : 'action restricted.'})
-            
-            // do delete query
-            const query = 'DELETE FROM users WHERE id = ?'
-            await connection.databaseQuery(query, id)
-            
-            // send feedback to client-side
-            res.status(200).send('user had been deleted.')
-        })
-    },
-    // multiple delete : input arry of id
-    multipleDelete : async (req, res) => {
-        await connection.databaseQueryWithErrorHandle(res, async () => {
-            // do authorization
-            if (req.user.role !== 'superadmin') throw ({code : 401, msg : 'action restricted.'})
-
-            // do query
-            const query = `DELETE FROM users WEHERE id IN (${[...req.body.id]})`
-            await connection.databaseQuery(query)
-            
-            // send feedback to client-side
-            res.status(200).send('users had been deleted.')
-        })
-    },
-
+    }
+    // superadmin get user data
 }
+
+// NOTE : all superadmin features need authentication and authorization
