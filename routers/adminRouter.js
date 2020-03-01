5bc745d7a48cf70a562de16f73c5ccccf247f51d
alee0510 <ali.muksin0510@gmail.com>
2020-03-01 20:41:54 +0700

    chain teh router

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -9,6 +9,12 @@ router.get('/get/users/', admin.paginationGetUsers) // need exception input, id 
 router.get('/get/users/next/', admin.paginationGetNextUsers)
 router.get('/get/users/prev/', admin.paginationGetPreviousUsers)
 router.get('/get/users/total', admin.getTotalUser)
+router.get('/get/users/profile/', admin.paginationGetUserProfile)
+router.get('/get/users/profile/next/', admin.paginationGetUserProfileNext)
+router.get('/get/users/profile/prev/', admin.paginationGetUserProfilePrev)
+router.get('/get/roles', admin.getUserRole)
+router.patch('/edit/roles/:id', admin.editUserRole)
+router.delete('/delete/users/:id', admin.deletUser)
 
 // export our routers
 module.exports = router
