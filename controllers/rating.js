c128127e1ba329a7adc120b12294b4e148fecac5
alee0510 <ali.muksin0510@gmail.com>
2020-03-06 00:31:51 +0700

    add more queries

--- controllers/rating.js
+++ controllers/rating.js
@@ -4,5 +4,76 @@ const connection = require('../helpers/databaseQuery')(database)
 
 // export controllers
 module.exports = {
-    // USER : get rating by user
+    // USER : GEt rating by user
+    getRatingByUser : (req, res) => {
+        connection.databaseQueryWithErrorHandle(res, async () => {
+            const query = 'SELECT * FROM ratings WHERE id ?'
+            const result = await connection.databaseQuery(query, parseInt(req.params.id))
+
+            // send feedback to client-side
+            res.status(200).send(result)
+        })
+    },
+    // SUPERADMIN and ADMIN : GET all ratings data
+    getInitialRatings : (req, res) => {
+        const limit = parseInt(req.query.limit)
+
+        // do athorization to define exception
+        const on = req.query.only === 'null' ? null : parseInt(req.query.only)
+        const only = on ? `WHERE pk.company_id = ${only}` : ''
+
+        connection.databaseQueryWithErrorHandle(res, async () => {
+            const query = `SELECT rt.id, us.username, rt.rating, rt.message, rt.date, pk.place_name 
+                        FROM ratings rt
+                        JOIN users us ON rt.user_id = us.id
+                        JOIN parking_area pk ON rt.area_id = pk.id
+                        ${only} ORDER BY rt.id DESC LIMIT ?`
+            const result = await connection.databaseQuery(query, limit)
+
+            // send feedback to client-side
+            res.status(200).send(result)
+        })
+    },
+    getNextRatings : (req, res) => {
+        const id = parseInt(req.query.id)
+        const limit = parseInt(req.query.limit)
+
+        // do athorization to define exception
+        const on = req.query.only === 'null' ? null : parseInt(req.query.only)
+        const only = on ? `pk.company_id = ${only}` : ''
+
+        connection.databaseQueryWithErrorHandle(res, async () => {
+            const query = `SELECT rt.id, us.username, rt.rating, rt.message, rt.date, pk.place_name 
+                        FROM ratings rt
+                        JOIN users us ON rt.user_id = us.id
+                        JOIN parking_area pk ON rt.area_id = pk.id
+                        WHERE ${only} id < ?
+                        ORDER BY rt.id DESC LIMIT ?`
+            const result = await connection.databaseQuery(query, [id, limit])
+
+            // send feedback to client-side
+            res.status(200).send(result)
+        })
+    },
+    getPrevRatings : (req, res) => {
+        const id = parseInt(req.query.id)
+        const limit = parseInt(req.query.limit)
+
+        // do athorization to define exception
+        const on = req.query.only === 'null' ? null : parseInt(req.query.only)
+        const only = on ? `pk.company_id = ${only}` : ''
+
+        connection.databaseQueryWithErrorHandle(res, async () => {
+            const query = `SELECT rt.id, us.username, rt.rating, rt.message, rt.date, pk.place_name 
+                        FROM ratings rt
+                        JOIN users us ON rt.user_id = us.id
+                        JOIN parking_area pk ON rt.area_id = pk.id
+                        WHERE ${only} id > ?
+                        ORDER BY rt.id ASC LIMIT ?`
+            const result = await connection.databaseQuery(query, [id, limit])
+
+            // send feedback to client-side
+            res.status(200).send(result)
+        })
+    }
 }
