65bc4e638b03580faf09f972028a98dc81044eaa
alee0510 <ali.muksin0510@gmail.com>
2020-03-06 14:20:53 +0700

    export all controllers

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,11 @@ const account = require('./account')
 const profile = require('./profile')
 const admin = require('./admin')
 const history = require('./history')
+const rating = require('./rating')
+const payment = require('./rating')
+const vehicles = require('./vehicles')
+const parking_area = require('./parking_area')
 
 // export controllers
-module.exports = { users, account, profile, admin, history }
+module.exports = { users, account, profile, admin, 
+    history, rating, payment, vehicles, parking_area }
